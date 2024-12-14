7aa2fd951d598b09809c58ab9bed10b0dc04d764
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-12-14 15:30:06 +0530

    restPassword controller defined,earlier was resetToken

--- server/controllers/resetPassword.js
+++ server/controllers/resetPassword.js
@@ -1,7 +1,8 @@
 const User = require("../models/User");
 const crypto=require('crypto');
 const mailSender = require("../utils/mailSender");
-exports.resetPassword=async(req,res)=>{
+const bcrypt=require('bcrypt');
+exports.resetPasswordToken=async(req,res)=>{
     try {
                 //fetch email
                 const{email}=req.body;    
@@ -40,10 +41,61 @@ exports.resetPassword=async(req,res)=>{
                      message:"mail for reseting password is sent successfully"
                 });
     } catch (error) {
-        console.log('Error in reset password', error); 
+        console.log('Error in reset password token', error); 
         return res.status(500).json({
             success: false,
-            message: 'failure in reseting password'
+            message: 'failure in reset password token'
+        });
+    }
+}
+
+exports.resetPassword=async(req,res)=>{
+    try {
+        //fetch data
+        const{token,password,confirmPassword}=req.body;
+        //validation and checking token
+        if(!token||!password||!confirmPassword){
+             return res.status(400).json({
+                 success:false,
+                 message:"all fields are required"
+            });
+        }
+        if(password!=confirmPassword){
+             return res.status(400).json({
+                 success:false,
+                 message:"passwords didnt match"
+            });
+        }
+        const tokenEntry=await User.findOne({resetToken:token});
+        if(!tokenEntry){
+             return res.status(404).json({
+                 success:false,
+                 message:"no token found,invalid token"
+            });
+        }
+        //checking token expiry
+        if(tokenEntry.tokenExpires<Date.now()){
+             return res.status(403).json({
+                 success:false,
+                 message:"token expired for resting password"
+            });
+        }
+        //hash password
+        const hashedPass=await bcrypt.hash(password,10);
+        //update password in db
+        const updatedUser=await User.findOneAndUpdate({token},
+            {password:hashedPass,resetToken:'',tokenExpires:''},{new:true});
+        
+         return res.status(200).json({
+             success:true,
+             message:"password reseted successfully"
+        });
+
+    } catch (error) {
+        console.log('error in resting password', error);
+        return res.status(500).json({
+            success: false,
+            message: 'failure in resetting password'
         });
     }
 }
