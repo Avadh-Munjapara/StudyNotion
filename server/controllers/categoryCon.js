6ab5b4adb68a6ff9a456319e1ebd512c57f88d06
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-12-19 11:16:48 +0530

    category page details handler defined

--- server/controllers/categoryCon.js
+++ server/controllers/categoryCon.js
@@ -1,4 +1,5 @@
-const Category=require('../models/Category')
+const Category=require('../models/Category');
+const Course = require('../models/Course');
 
 exports.createCategory=async (req,res)=>{
     try {
@@ -51,3 +52,35 @@ exports.getAllCategory=async(req,res)=>{
     }
 }
 
+exports.getCategoryPageDetails=async (req,res)=>{
+    try {
+        const {categoryId}=req.body;
+        const categoryCourses=await Category.findById(categoryId).populate("courses").select("courses");
+        if(!categoryCourses){
+             return res.status(400).json({
+                 success:false,
+                 message:"no category found"
+            });
+        }
+        if(categoryCourses.length==0){
+             return res.status(404).json({
+                 success:false,
+                 message:"no courses found for the selected category"
+        })
+    }
+    const diffCategoryCourses=await Category.find({name:{
+        $ne:categoryId 
+    }}).populate("courses").select("courses");
+    const topSellingCourses=await Course.find({}).sort({studentsEnrolled:"desc"}).limit(10);
+     return res.status(200).json({
+         success:true,
+         data:[categoryCourses,diffCategoryCourses,topSellingCourses]
+    }); 
+ } catch (error) {
+        console.log('error while fetching category page details', error);
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
