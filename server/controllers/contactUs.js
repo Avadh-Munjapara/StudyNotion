88b0bae527b2ecd25f0dcc50b8caebe0a9bd9156
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-12-19 17:48:59 +0530

    contactUs handler defined

---
+++ server/controllers/contactUs.js
@@ -0,0 +1,37 @@
+const mailSender=require('../utils/mailSender');
+require('dotenb').config();
+exports.contactUs=async(req,res)=>{
+    const{firstName,lastName,email,phoneNumber,message}=req.body;
+    if(!firstName||!lastName||!email||!phoneNumber||!message){
+         return res.status(400).json({
+             success:false,
+             message:"all fields are required"
+        });
+    }
+    try {
+        const contactAdmin=mailSender(process.env.USEREMAIL,"we have been contaced",
+            `the user${email} with name ${firstName} ${lastName} have reached us , and 
+            here its message:
+            ${message}`);
+    } catch (error) {
+        console.log('error while sending mail to admin', error);
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+    
+
+    try {
+        const contactUser=mailSender(email,"You have reached us",
+            `you respectfully ${firstName} and ${lastName} have reached us,we are grateful 
+            for your interest in our company,we will get back to you as soon as possible`);
+    } catch (error) {
+        console.log('error while sending mail to the user', error);
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+    
+}
