c5195658545ade60c25e3c7825644a0e9187bc2a
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-12-21 18:01:01 +0530

    for upadating dp and get enrolled courses controller defined

--- server/controllers/profileCon.js
+++ server/controllers/profileCon.js
@@ -1,6 +1,7 @@
 const Profile=require('../models/Profile');
 const User=require('../models/User');
-
+const { imageUpload } = require('../utils/cloudinaryUpload');
+require('dotenv').config();
 exports.updateProfile=async(req,res)=>{
     try {
        const{dob="",about="",phoneNumber,gender}=req.body; 
@@ -67,6 +68,56 @@ exports.getUserDetails=async(req,res)=>{
         })
     } catch (error) {
         console.log('error while getting user details', error);
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+exports.updateDisplayPicture=async(req,res)=>{
+    try {
+        const{displayPicture}=req.files;
+        const userId=req.user.id;
+        const dpCloudinary=await imageUpload(displayPicture,process.env.FOLDERNAME);
+        const updatedProfile=await User.findByIdAndUpdate(userId,{image:dpCloudinary.secure_url},{new:true});
+         return res.status(200).json({
+             success:true,
+             message:"display picture is updated successfully",
+             url:dpCloudinary.secure_url
+        });
+    } catch (error) {
+        console.log('error while updating the dp', error);
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+exports.getEnrolledCourses=async (req,res)=>{
+    try {
+        const userId=req.user.id;
+        const enrolledCourses=await User.findById(userId).populate("courses").select("courses");   
+        if(!enrolledCourses){
+             return res.status(404).json({
+                 success:false,
+                 message:"no user with that user id"
+            });
+        }
+        if(enrolledCourses.length==0){
+             return res.status(404).json({
+                 success:fasle,
+                 message:"you have not enrolled in any courses"
+            });
+        }
+         return res.status(200).json({
+             success:true,
+             message:"enrolled courses fetched successfully",
+             enrolledCourses
+        });
+    } catch (error) {
+        console.log('error while fetching enrolled courses data', error);
         return res.status(500).json({
             success: false,
             message: error.message
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,17 +2,28 @@ const express=require('express');
 const { signup, login, changePassword } = require('../controllers/auth');
 const {auth,isAdmin, isStudent}=require('../middlewares/auth');
 const { getCourseDetails } = require('../controllers/courseCon');
-const { getCategoryPageDetails } = require('../controllers/categoryCon');
+const { getCategoryPageDetails,createCategory } = require('../controllers/categoryCon');
 const { contactUs } = require('../controllers/contactUs');
+const {createSection, updateSection, deleteSection}=require('../controllers/sectionCon');
+const { createSubSection, updateSubSection, deleteSubSection } = require('../controllers/subSection');
 const router=express.Router;
 
 router.post("/sign-up",signup);
 router.post("/login",login);
-router.post("/change-password",changePassword);
 router.post("/change-password",changePassword);
 
 router.get("/course",auth,isStudent,getCourseDetails);
 router.get("/category-courses",auth,isStudent,getCategoryPageDetails);
 router.post('/contact-us',contactUs);
 
+router.post("/create-course",auth,isInstructor,createCourse);
+router.post("/create-section",auth,isInstructor,createSection);
+router.update("/update-section",auth,isInstructor,updateSection);
+router.delete("/delete-section",auth,isInstructor,deleteSection);
+ 
+router.post("/create-subSection",auth,isInstructor,createSubSection);
+router.update("/update-subSection",auth,isInstructor,updateSubSectionSubSection);
+router.delete("/delete-subSection",auth,isInstructor,deleteSubSection);
+
+router.post("/create-category",auth,isAdmin,createCategory);
 exports.module=router;
