f700e740e2cf69c21658af69ebb0d3de5edd15f4
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2025-05-19 17:30:52 +0530

    feat: implement CourseSidebar, SectionBarViewCourse, and SubSectionWindowViewCourse components with course details fetching and display functionality all for courseSideBar in viewCourse page

--- src/components/viewCourse/CourseSideBar.jsx
+++ src/components/viewCourse/CourseSideBar.jsx
@@ -1,33 +1,83 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { getFullCourseDetails } from '../../services/operations/courseApi';
-import { useDispatch } from 'react-redux';
-import { setCompletedLectures, setEntireCourseData, setSectionData, setTotalLectures } from '../../slices/viewCourse';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getFullCourseDetails } from "../../services/operations/courseApi";
+import { useDispatch, useSelector } from "react-redux";
+import Spinner from "../comman/Spinner";
+import SectionBarViewCourse from "./SectionBarViewCourse";
+import {
+  setCompletedLectures,
+  setEntireCourseData,
+  setSectionData,
+  setTotalLectures,
+} from "../../slices/viewCourse";
 const CourseSidebar = () => {
-    const {courseId, sectionId, subSectionId} = useParams();
-    const dispatch = useDispatch();
-    useEffect(() => {
-        const fetchCourseDetails = async () => {
-          const response = await getFullCourseDetails({ courseId }, dispatch);
-          if (response) {
-            console.log(response);
-            dispatch(setEntireCourseData(response));
-            dispatch(setSectionData(response?.courseContent));
-            const totalLectures= response?.courseContent?.reduce((acc,section)=>acc+section?.subSections?.length,0);
-            dispatch(setTotalLectures(totalLectures));
-            dispatch(setCompletedLectures(response?.completedLectures));
-
-          } else {
-            console.error("Failed to fetch course details");
+  const { courseId, sectionId, subSectionId } = useParams();
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.course.loading);
+  const sectionData = useSelector((state) => state.viewCourse.sectionData);
+  const entireCourseData = useSelector(
+    (state) => state.viewCourse.entireCourseData
+  );
+  const completedLectures = useSelector(
+    (state) => state.viewCourse.completedLectures
+  );
+  const totalLectures = useSelector((state) => state.viewCourse.totalLectures);
+  useEffect(() => {
+    const fetchCourseDetails = async () => {
+      const response = await getFullCourseDetails({ courseId }, dispatch);
+      if (response) {
+        console.log(response);
+        dispatch(setEntireCourseData(response));
+        dispatch(setSectionData(response?.courseContent));
+        const totalLectures = response?.courseContent?.reduce(
+          (acc, section) => acc + section?.subSections?.length,
+          0
+        );
+        dispatch(setTotalLectures(totalLectures));
+        dispatch(setCompletedLectures(response?.completedLectures));
+      } else {
+        console.error("Failed to fetch course details");
+      }
+    };
+    fetchCourseDetails();
+  }, []);
+  const detailsList = document.querySelectorAll("details");
+  detailsList.forEach((details) => {
+    details.addEventListener("toggle", () => {
+      if (details.open) {
+        detailsList.forEach((otherDetails) => {
+          if (otherDetails !== details) {
+            otherDetails.removeAttribute("open");
           }
-        };
-        fetchCourseDetails();
-      }, []);
-    return (
-        <div>
-            
-        </div>
-    );
-}
+        });
+      }
+    });
+  });
+
+  return loading ? (
+    <Spinner />
+  ) : sectionData.length > 0 ? (
+    <div className="bg-richblack-800  max-w-[300px] h-full">
+      <div className="border-b-[1px] pt-7 pb-3 border-richblack-600 mx-6">
+        <h1 className="text-sm font-semibold text-richblack-25">
+          {entireCourseData?.name}
+        </h1>
+        <p className="text-richblack-500 text-smm font-semibold">
+          <span>
+            {completedLectures?.length}/{totalLectures}
+          </span>
+        </p>
+      </div>
+      <div>
+        {sectionData?.map((section, index) => (
+          <SectionBarViewCourse section={section} />
+        ))}
+      </div>
+      {}
+    </div>
+  ) : (
+    <p>No Data Found</p>
+  );
+};
 
 export default CourseSidebar;
---
+++ src/components/viewCourse/SubSectionWindowViewCourse.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { MdOutlineMonitor } from "react-icons/md";
+import { useNavigate, useParams } from "react-router-dom";
+import { FaPlay } from "react-icons/fa";
+const SubSectionWindowViewCourse = ({ subSections }) => {
+  const subSectionId = useParams().subSectionId;
+  const navigate = useNavigate();
+  const completedLectures = useSelector(
+    (state) => state.viewCourse.completedLectures
+  );
+  const isLectureCompleted = (subSectionId) => {
+    return completedLectures.includes(subSectionId);
+  };
+  return (
+    <div className="text-sm px-6 flex flex-col py-3">
+      {subSections?.map((subSection, index) => {
+        const isCompleted = isLectureCompleted(subSection._id);
+        const isLoaded = subSectionId === subSection._id ? true : false;
+        console.log(isLoaded);
+        return (
+          <div
+            onClick={() =>
+              navigate(
+                `view-course/${subSection?.courseId}/sectionId/${subSection?.sectionId}/sub-sectionId/${subSection?._id}`
+              )
+            }
+            className="flex cursor-pointer items-center gap-2"
+          >
+            {isLoaded ? (
+              <FaPlay className="text-blue-100 " />
+            ) : (
+              <input
+                type="checkbox"
+                disabled
+                value={isCompleted}
+                name="isCompleted"
+                id="isCompleted"
+              />
+            )}
+
+            <h3
+              className={`${
+                isLoaded
+                  ? "text-blue-100 font-medium"
+                  : isCompleted
+                  ? "text-richblack-50 font-medium"
+                  : "text-richblack-300"
+              }`}
+            >
+              {subSection?.title}
+            </h3>
+            <MdOutlineMonitor
+              className={`${
+                isCompleted || isLoaded
+                  ? "text-richblack-50"
+                  : "text-richblack-300"
+              }`}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SubSectionWindowViewCourse;
