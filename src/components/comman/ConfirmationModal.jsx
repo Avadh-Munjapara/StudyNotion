8280ac3edbbad469e4906a324e97dd35bb7b8966
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2025-06-08 12:01:39 +0530

    refactor: enhance styling and layout of ConfirmationModal component

--- src/components/comman/ConfirmationModal.jsx
+++ src/components/comman/ConfirmationModal.jsx
@@ -1,38 +1,42 @@
-import React from 'react';
+import React from "react";
 
-const ConfirmationModal = ({heading,modalRef,btn1Text,btn2Text,btn1Handler,btn2Handler}) => {
-    return (
-        <>
-        <div className=" z-20 w-[100vw] absolute min-h-screen bg-white/10 top-0 left-0 bottom-0 right-0 backdrop-blur-sm "></div>
-        <div
-          ref={modalRef}
-          className="text-white fixed top-[16rem] left-[35%] font-medium w-fit border-white/70
-           shadow-[10px_10px_10px_rgba(0,0,0,1)] border-[1px] z-30
-          bg-richblack-800 h-fit px-10 rounded-md py-6 gap-3  flex   flex-col justify-center items-center "
-        >
-          <h2 className="text-lg">
+const ConfirmationModal = ({
+  heading,
+  modalRef,
+  btn1Text,
+  btn2Text,
+  btn1Handler,
+  btn2Handler,
+}) => {
+  return (
+    <div className="fixed inset-0 z-[1000] flex items-center bg-richblack-900/50 justify-center backdrop-blur-sm">
+      <div
+        ref={modalRef}
+        className="z-[1001] w-11/12 max-w-[350px] rounded-lg border-[1px] border-richblack-400 bg-richblack-800 p-6"
+      >
+        <h2 className="text-lg text-richblack-5 font-semibold text-center">
           Do you Really want to{" "}
-          <span className="text-red-600 font-semibold">{btn2Text}</span>
-          {" "}
+          <span className="text-pink-500">{btn2Text}</span>{" "}
           {heading && heading}?
-          </h2>
-          <div className="flex gap-3">
-            <button
+        </h2>
+        
+        <div className="mt-6 flex items-center justify-center gap-x-4">
+          <button
             onClick={btn1Handler}
-              className="px-4 py-2 border-[1px] border-white font-semibold text-black bg-yellow-200 rounded-md"
-            >
-              {btn1Text}
-            </button>
-            <button
-              onClick={btn2Handler}
-              className="px-4 py-2 border-[1px] border-white font-semibold text-black bg-richblack-300 rounded-md"
-            >
-              {btn2Text}
-            </button>
-          </div>
+            className="rounded-md bg-yellow-50 px-4 py-2 text-sm font-semibold text-richblack-900 hover:scale-95 transition-all duration-200"
+          >
+            {btn1Text}
+          </button>
+          <button
+            onClick={btn2Handler}
+            className="rounded-md bg-richblack-200 px-4 py-2 text-sm font-semibold text-richblack-900 hover:scale-95 transition-all duration-200"
+          >
+            {btn2Text}
+          </button>
         </div>
-      </>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default ConfirmationModal;
