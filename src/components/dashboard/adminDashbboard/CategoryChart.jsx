19d965fb3ae466dc5f4a0771061500dd468406ec
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2025-06-11 18:27:55 +0530

    added category chart component

---
+++ src/components/dashboard/adminDashbboard/CategoryChart.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const randomColors = (numColors) => {
+  let colors = [];
+  for (let i = 0; i < numColors; i++) {
+    const randomColor = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)},0.5)`;
+    colors.push(randomColor);
+  }
+  return colors;
+};
+const options = {
+  responsive: true,
+  maintainAspectRatio: true,
+  plugins: {
+    legend: {
+      position: "top",
+      labels: {
+        padding: 20,
+        color: "#AFB2BF", // richblack-300
+      },
+    },
+  },
+};
+const CategoryChart = ({ categoryData }) => {
+  const data = {
+    labels: categoryData?.map((category) => category.name),
+    datasets: [
+      {
+        label: "students",
+        data: categoryData?.map((category) => category.totalStudents),
+        backgroundColor: randomColors(categoryData?.length),
+        hoverOffset: 20
+      },
+
+    ],
+  };
+  return (
+    <div className="h-fit">
+      <h2>Student's favourite category</h2>
+      <div>
+        <Pie
+          options={{
+            offset: 0,
+            maintainAspectRatio: false,
+          }}
+          height={"300"}
+          data={data}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CategoryChart;
--- src/pages/dashboardPages/AdminPages/AdminDash.jsx
+++ src/pages/dashboardPages/AdminPages/AdminDash.jsx
@@ -1,23 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { getAdminDashboardDetails } from '../../../services/operations/adminapi';
-import { useSelector } from 'react-redux';
-
+import React, { useEffect, useState } from "react";
+import { getAdminDashboardDetails } from "../../../services/operations/adminapi";
+import { useSelector } from "react-redux";
+import Spinner from "../../../components/comman/Spinner";
+import CategoryChart from "../../../components/dashboard/adminDashbboard/CategoryChart";
 const AdminDash = () => {
-    const [loading,setLoading]=useState(false);
-    const token=useSelector((state)=>state.auth.token);
-    useEffect(()=>{
-        const fetchData=async (token)=>{
-            const response=await getAdminDashboardDetails(token,setLoading);
-            if(response)console.log(response,"response");
-        }
-        fetchData(token);
-    },[]);
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const [loading, setLoading] = useState(false);
+  const token = useSelector((state) => state.auth.token);
+  const [mstSoldCrs, setMstSoldCrs] = useState([]);
+  const [catWiseCrs, setCatWiseCrs] = useState([]);
+  const [topIns, setTopIns] = useState([]);
+  const [stats, setStats] = useState([]);
+  useEffect(() => {
+    const fetchData = async (token) => {
+      const response = await getAdminDashboardDetails(token, setLoading);
+      if (response) {
+        setCatWiseCrs(response?.categoryWiseCourses);
+        setMstSoldCrs(response?.mostSellingCourses);
+        setStats(response?.stats);
+        setTopIns(response?.topInstructors);
+      }
+    };
+    fetchData(token);
+  }, []);
+  return loading 
+  ? <Spinner /> 
+  : <div className="">
+    <h1>Admin DashBoard</h1>
+    <div className="grid grid-cols-2 grid-rows-2">
+        <CategoryChart categoryData={catWiseCrs}/>
+    </div>
+
+  </div>;
+};
 
 export default AdminDash;
- 
